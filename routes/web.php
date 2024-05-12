910fb8edb44ba093c593168fe2d17a7ca2a57461
KtranH <87740741+KtranH@users.noreply.github.com>
2024-05-12 17:35:27 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,10 @@ use App\Http\Controllers\HomeUser;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeUser::class, 'LoadActivity']);
+Route::get('/login', function () {
+    return view('Account.login');
+});
+Route::get('/signup', function () {
+    return view('Account.signUp');
+});
+
